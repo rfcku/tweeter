be61e3ab726fce4a73f5f2169d8ad58845a27be6
rfcku <rafackyou@gmail.com>
2023-01-22 22:08:59 -0800

    fix: component import

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import styles from "@/styles/Home.module.css";
-import Tweet from "@/components/Tweet";
+import styles from "../styles/Home.module.css";
+import Tweet from "../components/Tweet";
 import Navbar from "../components/Navbar";
 import useSmartTweet from "../hooks/useSmartTweet";
 
--- pages/pricing.js
+++ pages/pricing.js
@@ -2,8 +2,8 @@ import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-import Tweet from "@/components/Tweet";
+import styles from "../styles/Home.module.css";
+import Tweet from "../components/Tweet";
 
 import Navbar from "../components/Navbar";
 
