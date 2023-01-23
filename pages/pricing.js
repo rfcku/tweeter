f3dc4e2f6bff4034c5d2dce4cfaff9e2a60f2b80
rfcku <rafackyou@gmail.com>
2023-01-22 22:18:29 -0800

    fix: tweet import

--- pages/pricing.js
+++ pages/pricing.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
-import Tweet from "../components/Tweet";
+import Tweet from "../components/tweet";
 
 import Navbar from "../components/Navbar";
 
